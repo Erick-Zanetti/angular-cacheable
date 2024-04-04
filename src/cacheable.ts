cf35ae92cc487195ab51cf6336ea4070c9263ae7
Erick Zanetti <erick_98zanetti_98@hotmail.com>
2024-04-04 13:23:12 -0300

    remove logs

--- src/cacheable.ts
+++ src/cacheable.ts
@@ -12,21 +12,17 @@ export function Cacheable(timer?: number): MethodDecorator {
 
         descriptor.value = function (...args: any[]) {
             const cacheKey = propertyName.toString() + '_' + JSON.stringify(args);
-            console.log(cacheKey);
 
             const cache = CacheService.get(cacheKey);
             if (cache) {
                 if (cache.isCompleted()) {
-                    console.log('by completed');
                     return cache.buffer;
                 } else {
-                    console.log('by buffer');
                     return cache.buffer;
                 }
             } else {
                 const cache = new RequestCache();
                 CacheService.add(cacheKey, cache);
-                console.log('no cache');
 
                 const result = originalMethod.apply(this, args);
                 return result.pipe(
